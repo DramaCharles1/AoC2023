b55f7c93075042b4651622c1d2221525824d25f1
Richard Stuart <richard.niklas.stuart@gmail.com>
2023-12-04 22:53:49 +0100

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -79,6 +79,72 @@ fn each_game(game: &str) -> i32{
     return game_id;
 }
 
+fn each_game2(game: &str) -> i32{
+    //for each round:
+    //Find highest amount of each color
+    let mut red_amount: i32 = 0;
+    let mut green_amount: i32 = 0;
+    let mut blue_amount: i32 = 0;
+
+    println!("{}", game);
+    let substrings_game_id: Vec<&str> = game.split(':').collect();
+    let substrings_split_comma: Vec<&str> = substrings_game_id[1].split(';').collect();
+
+    for substring in &substrings_split_comma {
+        //println!("round: {}", substring);
+        if substring.contains(',') {
+            let substring_split_color: Vec<&str> = substring.split(',').collect();
+            for subtring_color in &substring_split_color {
+                let digit_and_color: Vec<&str> = subtring_color.trim().split(' ').collect();
+                let amount: i32 = digit_and_color[0].parse::<i32>().unwrap();
+                //println!("digit and color: {}", digit_and_color[1]);
+                if digit_and_color[1].contains("red") {
+                    if amount > red_amount {
+                        //println!("New high red amount: {}", amount);
+                        red_amount = amount;
+                    }
+                } else if digit_and_color[1].contains("green") {
+                    if amount > green_amount {
+                        //println!("New high green amount: {}", amount);
+                        green_amount = amount;
+                    }
+                } else {
+                    if amount > blue_amount {
+                        //println!("New high blue amount: {}", amount);
+                        blue_amount = amount;
+                    }
+                }
+            }
+        } else {
+            let digit_and_color: Vec<&str> = substring.trim().split(' ').collect();
+            let amount: i32 = digit_and_color[0].parse::<i32>().unwrap();
+            //println!("amount: {}", amount);
+            if digit_and_color[1].contains("red") {
+                if amount > red_amount {
+                    //println!("New high red amount: {}", amount);
+                    red_amount = amount;
+                }
+            } else if digit_and_color[1].contains("green") {
+                if amount > green_amount {
+                    //println!("New high green amount: {}", amount);
+                    green_amount = amount;
+                }
+            } else {
+                if amount > blue_amount {
+                    //println!("New high blue amount: {}", amount);
+                    blue_amount = amount;
+                }
+            }
+        }
+    }
+    println!("Red amount: {}", red_amount);
+    println!("Green amount: {}", green_amount);
+    println!("Blue amount: {}", blue_amount);
+    let sum: i32 = red_amount * green_amount * blue_amount;
+    println!("Sum from game: {}", sum);
+    return sum;
+}
+
 fn main() {
     let mut read_lines = Vec::new();
     match read_file_lines("input.txt") {
@@ -92,14 +158,9 @@ fn main() {
 
     let mut sum: i32 = 0;
     for line in &read_lines {
-        sum = sum + each_game(line);
+        sum = sum + each_game2(line);
     }
     println!("Sum: {}", sum);
-    //Max red = 12
-    //Max green = 13
-    //Max blue = 14
-
-
 }
 
 
