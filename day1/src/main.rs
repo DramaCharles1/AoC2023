936a7a2f6dd131e5c73f218b415a3e2dc9532991
Stuart <richard.stuart@altramotion.com>
2023-12-07 12:48:27 +0100

    day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,41 @@
-//use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead};
+
+fn replace_digit_string_with_int(mut line: String) -> String{
+    let string_vector: Vec<String> = vec![
+        String::from("zero"),
+        String::from("one"),
+        String::from("two"),
+        String::from("three"),
+        String::from("four"),
+        String::from("five"),
+        String::from("six"),
+        String::from("seven"),
+        String::from("eight"),
+        String::from("nine"),
+    ];
+
+    let number_vector: Vec<String> = vec![
+        String::from("zero0zero"),
+        String::from("one1one"),
+        String::from("two2two"),
+        String::from("three3three"),
+        String::from("four4four"),
+        String::from("five5five"),
+        String::from("six6six"),
+        String::from("seven7seven"),
+        String::from("eight8eight"),
+        String::from("nine9nine"),
+    ];
+
+    for i in 0..string_vector.len() {
+        if line.contains(&string_vector[i]) {
+            line = line.replace(&string_vector[i], &number_vector[i]);
+        }
+    }
+    return line;
+}
+
 fn main() -> io::Result<()> {
     // let file_contents = fs::read_to_string("example.txt")
     //     .expect("LogRocket: Should have been able to read the file");
@@ -12,25 +47,25 @@ fn main() -> io::Result<()> {
     for line in reader.lines() {
         // Handle each line as needed
         match line {
-            Ok(line_content) => {
+            Ok(mut line_content) => {
                 
                 let mut my_vector: Vec<char> = Vec::new();
                 println!("Line: {}", line_content);
                 // Do something with the line content
+                line_content = replace_digit_string_with_int(line_content);
+                println!("modified line: {}", line_content);
                 let chars: Vec<char> = line_content.chars().collect();
                 if let Some(first_digit) = chars.iter().find(|&c| c.is_digit(10)) {
                     my_vector.push(*first_digit);
                     let digit: u32 = first_digit.to_digit(10).unwrap();
-                    println!("The first digit is: {}", digit);
-                    //my_vector.push(digit);
+                    //println!("The first digit is: {}", digit);
                 }else{
                     println!("No digit found in the string.");
                 }
                 if let Some(last_digit) = chars.iter().rev().find(|&c| c.is_digit(10)) {
                     my_vector.push(*last_digit);
                     let digit: u32 = last_digit.to_digit(10).unwrap();
-                    println!("The last digit is: {}", digit);
-                    //my_vector.push(digit);
+                    //println!("The last digit is: {}", digit);
                 }else{
                     println!("No digit found in the string.");
                 }
@@ -46,8 +81,6 @@ fn main() -> io::Result<()> {
     }
     println!("sum: {}", sum);
     Ok(())
-
-    //let chars: Vec<char> = input_string.chars().collect();
 
     //Readln
     //get first digit from line
